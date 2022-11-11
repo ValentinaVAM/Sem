151cd881d5bab85271c0cff242d07f248653deb5
Valiantsina <valentinass@yandex.ru>
2022-11-11 20:11:29 +0300

    Example007

---
+++ Example007_Magic/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+
+int xa = 40, ya = 1,
+    xb = 1, yb = 10,
+    xc = 80, yc = 10;
+
+Console.SetCursorPosition(xa, ya);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(xb, yb);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(xc, yc);
+Console.WriteLine("+");
