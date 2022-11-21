a0f7311b6e14229e5b045174ce82e2a06561387a
Valiantsina <valentinass@yandex.ru>
2022-11-21 23:15:40 +0300

    Example012

---
+++ Example012_5/Program.cs
@@ -0,0 +1,13 @@
+﻿// Цикл for
+
+string Method4(int count, string text)
+{
+    string result = string.Empty;
+    for(int i = 0; i < count; i++)
+    {
+        result = result + text;
+    }
+    return result;
+}
+string res = Method4(10, "D");
+Console.WriteLine(res);
