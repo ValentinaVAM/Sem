2c2b4568f28a2319c5508b484352f22a3312b4ab
Valiantsina <valentinass@yandex.ru>
2022-11-21 21:59:25 +0300

    Example012

--- Example009_IntroArray/Program.cs
+++ Example009_IntroArray/Program.cs
@@ -6,7 +6,7 @@
     return result;
 }
 
-int[]array = { 11, 211, 31, 41, 15, 61, 17, 18, 19 };
+int[]array = { 11, 211, 31, 41, 1502, 61, 17, 18, 19 };
 
 int result = Max(Max(array[0], array[1], array[2]), Max(array[3], array[4], array[5]), Max(array[6], array[7], array[8]));
 Console.WriteLine(result);
---
+++ Example012_4/Program.cs
@@ -0,0 +1,17 @@
+﻿// Вид 4
+
+string Method4(int count, string c)
+{
+    int i = 0;
+    string result = "";
+
+    while(i < count)
+    {
+        result = result + c;
+        i++;
+    }
+    return result;
+}
+
+string res = Method4(10, "asdf");
+Console.WriteLine(res);
